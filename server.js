5641e82b994bc5493ed423386870c5e7a252ffa2
aswinen-17 <enaswin17@gmail.com>
2026-01-16 23:05:07 +0530

    fixed production frontend serving

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const colors = require("colors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const errorHandler = require("./middlewares/errormiddleware");
-const authRoutes = require('./routes/authRoutes');
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 
@@ -18,29 +18,33 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-app.use(errorHandler);
 
 const PORT = process.env.PORT || 8080;
 
+
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/openai", require("./routes/openaiRoutes"));
+
 
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "client/build")));
 
-  app.get("*", (req, res) => {
+  app.use((req, res) => {
     res.sendFile(
       path.resolve(__dirname1, "client", "build", "index.html")
     );
   });
 }
 
+
+app.use(errorHandler);
+
 app.listen(PORT, () => {
   console.log(
-    `Server Running in ${process.env.DEV_MODE} mode on port no ${PORT}`.bgCyan
-      .white
+    `Server Running in ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white
   );
 });
